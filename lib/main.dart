8a9246e6badf8c16f2a66893d6b3d1ad8dde1cbe
Nyabayo <ernestosindo9@gmail.com>
2024-05-20 09:42:33 +0300

    container

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+
 import 'package:demo_app/on_boarding.dart';
 import 'package:flutter/material.dart';
 
@@ -11,8 +12,8 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home:const OnBoardingWelcomeScreen(),
+    return const MaterialApp(
+      home: OnBoardingWelcomeScreen(),
     );
   }
 }
