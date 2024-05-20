6fc13778e3b116a4f27afaad67a866a3a79811fa
Nyabayo <ernestosindo9@gmail.com>
2024-05-20 08:05:31 +0300

    first commit

---
+++ lib/on_boarding.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class OnBoardingWelcomeScreen extends StatelessWidget {
+  const OnBoardingWelcomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.green,
+        //ensure we view the top bar views e.g notification icon(safe area)
+        body: Container(
+          height: 100,
+          width: 100,
+          color: Colors.red,git init
+        )
+        
+      );
+  }
+}
